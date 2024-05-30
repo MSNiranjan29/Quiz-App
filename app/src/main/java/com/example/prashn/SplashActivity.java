ed489c07a1788fb893be9894f4761183e39480fa
MSNiranjan29 <adeptmohitsingh@outlook.com>
2024-05-30 10:25:32 +0530

    Initial commit

---
+++ app/src/main/java/com/example/prashn/SplashActivity.java
@@ -0,0 +1,54 @@
+package com.example.prashn;
+
+import android.content.Intent;
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.res.ResourcesCompat;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class SplashActivity extends AppCompatActivity {
+    private TextView appName;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_splash);
+
+        appName = findViewById(R.id.app_name);
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        // Move the remaining setup code outside of the WindowInsets listener
+        Typeface typeface = ResourcesCompat.getFont(this, R.font.fstyle);
+        appName.setTypeface(typeface);
+
+        Animation anim = AnimationUtils.loadAnimation(this, R.anim.myanim);
+        appName.setAnimation(anim);
+
+        new Thread() {
+            public void run() {
+                try {
+                    sleep(3000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }.start();
+    }
+}
